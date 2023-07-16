6f79db63b12b8e263a8b4ba8b72e15bcc2775369
RMosley912 <rmosley241@gmail.com>
2023-07-16 12:04:58 -0400

    push

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,58 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+  $(function () {
+
+    // Display current date in the header
+    var today = dayjs();
+    $('#currentDay').text(today.format("MMM D, YYYY"));
+  
+    // Load any saved events from local storage
+    $(".time-block").each(function() {
+      var id = $(this).attr("id");
+      var savedEvent = localStorage.getItem(id);
+  
+      if (savedEvent !== null) {
+        $(this).children(".description").val(savedEvent);
+      }
+    });
+  
+    // Color code time blocks based on current time
+    function colorCode() {
+      var currentHour = dayjs().hour();
+    
+      $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    
+        if (blockHour < currentHour) {
+          $(this).addClass("past");
+        } 
+        else if (blockHour === currentHour) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        } 
+        else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+      });
+    }
+  
+    colorCode();
+  
+    // Save button event listener
+    $(".saveBtn").click(function() {
+      var timeBlock = $(this).parent();
+      var hour = timeBlock.attr("id");
+      var eventText = timeBlock.children(".description").val();
+  
+      localStorage.setItem(hour, eventText);
+    });
+  });
+  
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +71,5 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
+
